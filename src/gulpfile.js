013cc912dbd568eb1d9e3f4189db427e2c672e3c
mxfc <339583983@qq.com>
2016-12-08 18:21:58 +0800

    首页大致布局

---
+++ src/gulpfile.js
@@ -0,0 +1,189 @@
+'use strict';
+// 引入依赖组件
+const gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    cssmin = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    jsmin = require('gulp-uglify'),
+    imgmin = require('gulp-imagemin'),
+    pngmin = require('imagemin-pngquant'),
+    rev_append = require('gulp-rev-append'),
+    prefix = require('gulp-autoprefixer'),
+    md5_suffix = require('gulp-make-css-url-version'),
+    less = require('gulp-less'),
+    maps = require('gulp-sourcemaps'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    clean = require('gulp-clean'),
+    sequence = require('gulp-sequence'),
+    browser_sync = require('browser-sync').create();
+
+// 配置对象
+const cfg = (() => {
+    let _src = './', _dev = '../dev/', _build = '../build/';
+    return {
+        src: _src,
+        dev: _dev,
+        build: _build,
+        server: _dev,
+        server_build: _build,
+        port: 5222,
+        startPath: "/html/index.html",
+        less_src: [_src + 'less/*.less'],
+        less_watch: [_src + 'less/**/*.less'],
+        less_to: _dev + '/css',
+        concat_src: [_src + 'js/zepto.js', _src + 'js/base.js', _src + 'js/data_module.js', _src + 'js/sm.js', _src + 'js/sm-extend.js', _src + 'js/init.js', _src + 'js/page/*.js'],
+        concat_src2build: [_src + 'js/zepto.js', _src + 'js/base.js', _src + 'js/sm.js', _src + 'js/sm-extend.js', _src + 'js/init.js', _src + 'js/page/*.js'],
+        concat_to: _dev + '/js',
+        htmlmin_src: [_dev + '*.html', _dev + 'html/**/*.html'],
+        htmlmin_to: _build + '/html',
+        copy2dev: [_src + 'font/**/*.{eot,ttf}',
+                    _src + 'css/**/*.css',
+                    _src + 'html/**/*.html',
+                    _src + 'json/**/*.json',
+                    _src + 'document/**/*.md',
+                    _src + 'img/**/*.{png,jpg,gif,ico}',
+                    _src + '*.{js,css,json,png,jpg,gif,ico}',
+                    '!' + _src + 'package.json',
+                    '!' + _src + 'gulpfile.js'],
+        copy2build: [_dev + 'font/**/*.{eot,ttf}', _dev + 'document/**/*.md', _dev + 'ui.json'],
+        cssmin_src: [_dev + 'css/**/*.css'],
+        cssmin_to: _build + '/css/',
+        imgmin_src: [_dev + 'img/**/*.{png,jpg,gif,ico}'],
+        imgmin_to: _build + '/img/',
+        jsmin_src: [_dev + 'js/**/*.js'],
+        jsmin_to: _build + '/js/',
+        clean_dev: [_dev + '*'],
+        clean_build: [_build + '*', '!' + _build + "/.git"]
+    };
+})();
+
+// 清除dev
+gulp.task('clean_dev', () => {
+    return gulp.src(cfg.clean_dev, {read: false})
+    .pipe(clean({force: true}));
+});
+
+// 复制无需编译的代码 到 dev
+gulp.task('copy2dev', function(){
+    return gulp.src(cfg.copy2dev, { base: cfg.src })
+    .pipe(changed(cfg.dev))
+    .pipe(gulp.dest(cfg.dev));
+});
+
+// 编译less为css 生成sourceMaps 添加厂商前缀 MD5后缀
+gulp.task('less', () => {
+    return gulp.src(cfg.less_src)
+    .pipe(maps.init())
+    .pipe(less())
+    .pipe(prefix({
+        browsers: ['Android >= 4', 'Chrome >= 40', 'iOS >= 8', 'Safari >= 6']
+    }))
+    .pipe(md5_suffix())
+    .pipe(maps.write('../' + cfg.dev + '/maps_css', {sourceMappingURL: file => {
+            return '/maps_css/' + file.relative + '.map';
+        }}))
+    .pipe(gulp.dest(cfg.less_to))
+    .pipe(browser_sync.stream());
+});
+// js代码拼接
+gulp.task('concat',function(){
+    var is_build = process.argv[2] && (process.argv[2] === "build");
+    return gulp.src(is_build ? cfg.concat_src2build : cfg.concat_src)
+    .pipe(maps.init())
+    .pipe(concat('base.js'))
+    .pipe(maps.write('../' + cfg.dev + '/maps_js', {sourceMappingURL: file => {
+            return '/maps_js/' + file.relative + '.map';
+        }}))
+    .pipe(gulp.dest(cfg.concat_to));
+});
+// 监听
+gulp.task('watch',function(){
+    gulp.watch(cfg.less_watch,['less']);
+    gulp.watch(cfg.concat_src,['concat',browser_sync.reload]);
+    gulp.watch(cfg.copy2dev,['copy2dev',browser_sync.reload]);
+});
+
+// 给html中的url设置MD5后缀以及压缩html
+gulp.task('htmlmin', function(){
+    var opts = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    return gulp.src(cfg.htmlmin_src)
+    .pipe(rev_append())
+    .pipe(htmlmin(opts))
+    .pipe(gulp.dest(cfg.htmlmin_to));
+});
+// 给css添加浏览器厂商前缀、css中的url添加MD5后缀、压缩css
+gulp.task('cssmin',['less'],function(){
+    return gulp.src(cfg.cssmin_src)
+    .pipe(cssmin({
+        advanced: true,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+        compatibility: '*',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+        keepBreaks: false//类型：Boolean 默认：false [是否保留换行]
+    }))
+    .pipe(gulp.dest(cfg.cssmin_to));
+});
+// js代码压缩
+gulp.task('jsmin',function(){
+    return gulp.src(cfg.jsmin_src)
+    .pipe(jsmin({
+        //mangle: true,//类型：Boolean 默认：true 是否修改变量名
+        mangle: {except: ['require' ,'exports' ,'module' ,'$']}//排除混淆关键字
+    }))
+    .pipe(gulp.dest(cfg.jsmin_to));
+});
+// 图片压缩、png图的深度压缩
+gulp.task('imgmin',function(){
+    return gulp.src(cfg.imgmin_src)
+    .pipe(cache(imgmin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化,
+            svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
+            use: [pngmin()] //使用pngquant深度压缩png图片的imagemin插件
+        })))
+    .pipe(gulp.dest(cfg.imgmin_to));
+});
+// 清除build
+gulp.task('clean_build', () => {
+    return gulp.src(cfg.clean_build, {read: false})
+    .pipe(clean({force: true}));
+});
+
+// 复制无需编译的代码 到 build
+gulp.task('copy2build', function(){
+    return gulp.src(cfg.copy2build, { base: cfg.dev })
+    .pipe(gulp.dest(cfg.build));
+});
+
+
+gulp.task('default', function(){
+    sequence('clean_dev',['less','concat','copy2dev'],'watch',() => {
+        browser_sync.init({
+            server: cfg.server,
+            port: cfg.port,
+            open: "external",
+            startPath: cfg.startPath
+        });
+    });
+});
+
+gulp.task('build', function(){
+    sequence(['clean_dev'],['less','concat','copy2dev'],'clean_build',['htmlmin', 'cssmin', 'jsmin', 'imgmin', 'copy2build'],() => {
+        browser_sync.init({
+            server: cfg.server_build,
+            port: cfg.port,
+            open: "external",
+            startPath: cfg.startPath
+        });
+    });
+});
---
+++ src/js/base.js
@@ -0,0 +1,111 @@
+// 全局配置
+window.cfg_lz = {
+    native_timeout: 1200                    //原生接口调用延迟
+};
+(function(){
+    window.base = {
+        apple_url: 'https://itunes.apple.com/cn/app/youbini-laite/id1149698186?mt=8',   //苹果app更新地址
+        native_call_times: {},              //原生接口调用时间
+        callback: {},                       //所有原生方法的回调函数的容器
+        link_title: {                       //页面标题
+            personal: '个人中心',
+            index: '首页'
+        },
+        addLoading: function(){             //添加页面Loding
+            var _html = '<div class="loading">'
+                +'<div class="loading_box">'
+                    +'<i class="loading_circle"></i>'
+                +'</div>'
+            +'</div>';
+            $('body').prepend(_html);
+        },
+        removeLoading: function(time){      //取消页面Loding
+            setTimeout(function(){
+                $('.loading').remove();
+            },time || 0);
+        },
+        dataCheck: function(data){          //对原生应用返回数据的检测
+            // 非对象数据一律认定为格式错误
+            if($.type(data) !== 'object'){
+                $.alert('网络故障<br>(response data is not json)');
+                return false;
+            }
+            if(+data.errcode === 0){
+                $.modal({
+                    title: '网络异常',
+                    buttons: [{
+                        text: '重试',
+                        onClick: function() {
+                            base.native.recent_call();
+                        }
+                    }]
+                });
+                return false;
+            }else if(+data.errcode === 1){
+                $.alert(data.msg || "网络故障<br>(errcode: 1)");
+                return false;
+            }
+            return true;
+        },
+        native: function(fn_name, obj){     //原生方法调用（1.方法名 2.传的参数或回调函数）
+            // 最近一次原生调用记忆
+            base.native.recent_call = (function(fn_name, obj){
+                if(fn_name === "render"){
+                    base.data_refresh = obj.callback;
+                }
+                return function(){
+                    base.native(fn_name, obj);
+                }
+            })(fn_name, $.extend({},typeof obj === "function" ? {callback: obj} : obj));
+            // 接口防暴力调用
+            var _now_time = +new Date;
+            if(base.native_call_times[fn_name] && _now_time - base.native_call_times[fn_name] < cfg_lz.native_timeout){
+                $.toast('操作频繁');
+                return false;
+            }
+            base.native_call_times[fn_name] = _now_time;
+
+            // 回调函数封装
+            var _cb_name = fn_name+'_'+_now_time;
+            var _fn;
+            if($.type(obj) === "function"){
+                _fn = obj;
+                obj = {};
+            }else if($.type(obj) === "object" && obj.callback){
+                _fn = obj.callback;
+            }
+
+            if(_fn !== undefined){
+                base.callback[_cb_name] = function(data){
+                    // 检查数据是否异常
+                    if(base.dataCheck(data)){
+                        _fn(data);
+                    }
+                    // 方法调用完删除自身，避免无限增加内存溢出
+                    delete base.callback[_cb_name];
+                };
+                obj.callback = 'base.callback.'+_cb_name;
+            }
+
+            // 添加页面标题
+            if(fn_name === 'page_to' && obj.link){
+                obj.title = base.link_title[obj.link] || obj.title || '<%= activity %>';
+            }
+
+            // 原生调用差异性整合封装
+            if(window.native_android){
+                window.native_android[fn_name](JSON.stringify(obj));
+            }else if(window.webkit && window.webkit.messageHandlers){
+                window.webkit.messageHandlers[fn_name].postMessage(obj || '');
+            }else{
+                window.native[fn_name](obj);
+            }
+        },
+        refresh: function(flag){
+            // 页面刷新的后续操作
+            var tips = ["<%= refresh_fail %>", '<%= refresh_success %>']
+            $.pullToRefreshDone('.pull-to-refresh-content');
+            $.toast(tips[flag]);
+        }
+    };
+})();
